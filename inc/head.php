ac2d682a34a0a14737c340c95a7b92b0ae5382a0
Sharad Shinde <shindesharad71@gmail.com>
2016-07-21 06:32:59 +0530

    styles updated for navbar

--- inc/head.php
+++ inc/head.php
@@ -8,11 +8,11 @@ echo '
 <head>
 	<title>'.$title.'</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="../css/bootstrap.min.css" rel="stylesheet">
-  	<script src="../js/jquery.js"></script> 
-  	<script src="../js/bootstrap.min.js"></script> 
-  	<link href="../editor/summernote.css" rel="stylesheet">
-  	<script src="../editor/editor.js"></script>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+ 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.css" rel="stylesheet">
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.js"></script>
 	<link rel="stylesheet" href="../css/styles.css">	
 </head>
 <body>
