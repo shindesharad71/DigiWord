e386d92c72f1abb875f1132d9cc0d3c40df1a907
Sharad Shinde <shindesharad71@gmail.com>
2016-07-21 06:17:34 +0530

    mobile navvbar search bug fixed

--- inc/head.php
+++ inc/head.php
@@ -8,11 +8,11 @@ echo '
 <head>
 	<title>'.$title.'</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  	<link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.css" rel="stylesheet">
-	<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.js"></script>
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
+  	<script src="../js/jquery.js"></script> 
+  	<script src="../js/bootstrap.min.js"></script> 
+  	<link href="../editor/summernote.css" rel="stylesheet">
+  	<script src="../editor/editor.js"></script>
 	<link rel="stylesheet" href="../css/styles.css">	
 </head>
 <body>
@@ -25,8 +25,21 @@ echo '
         				<span class="icon-bar"></span>
         				<span class="icon-bar"></span> 
      				 </button>
+     				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#searchbar">
+        				<i class="glyphicon glyphicon-search white"></i>
+     				 </button>
       				<a class="navbar-brand" id="navtitle" href="./">'.BLOG.'</a>
     			</div>
+    			<div class="collapse navbar-collapse pull-right" id="searchbar">
+        			<form class="navbar-form" role="search" action="http://localhost/search.php">
+        				<div class="input-group">
+            				<input type="text" class="form-control" placeholder="Search" name="term" id="term" required>
+            			<div class="input-group-btn">
+                			<button class="btn btn-default form-control" type="submit"><i class="glyphicon glyphicon-search" name="search" id="search"></i></button>
+            			</div>
+        				</div>
+       				</form>
+        		</div>
     			<div class="collapse navbar-collapse" id="myNavbar">
 	    			<ul class="nav navbar-nav navbar-right">';
 	    			if(isset($_SESSION['username']))
@@ -38,16 +51,6 @@ echo '
 		    				<li><a href="categories.php">Categories</a></li>
 		     				<li><a href="logout.php" id="logout">Logout</a></li>';
 	    			}
-				echo '</ul>
-				<div class="col-sm-3 col-md-3 pull-right">
-        			<form class="navbar-form" role="search" action="../search.php">
-        				<div class="input-group">
-            				<input type="text" class="form-control" placeholder="Search" name="term" id="term" required>
-            			<div class="input-group-btn">
-                			<button class="btn btn-default form-control" type="submit"><i class="glyphicon glyphicon-search" name="search" id="search"></i></button>
-            			</div>
-        				</div>
-       				</form>
-        		</div>
-				</div></div></nav></header>';
+				echo '</ul></div>
+				</div></nav></header>';
 ?>
